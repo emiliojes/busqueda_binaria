e7f440161fc783e21efed4cc6ee4f6f645e617bd
Emilio Nuñez <emiliojes@gmail.com>
2020-07-20 17:34:42 -0500

    Corrigiendo algunas espacios entre el print en la concatenacion

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -1,6 +1,6 @@
 import random
 
-def busqueda_binaria(lista, comienzo, final, objetivo)
+def busqueda_binaria(lista, comienzo, final, objetivo):
     if comienzo > final:
         return False
     
@@ -16,8 +16,8 @@ def busqueda_binaria(lista, comienzo, final, objetivo)
 
 
 if __name__ == '__main__':
-    tamano_de_lista = int(input('De que tamano es la lista?'))
-    objetivo = int(input('Que numero quieres encontrar'))
+    tamano_de_lista = int(input('De que tamano es la lista?: '))
+    objetivo = int(input('Que numero quieres encontrar?: '))
 
     lista = sorted([random.randint(0,100) for i in range(tamano_de_lista)])
 
@@ -25,4 +25,4 @@ if __name__ == '__main__':
 
     print(lista)
 
-    print(f'El elemento {objetivo}{"esta" if encontrado else "no esta"} en la lista')
+    print(f'El elemento {objetivo}{" esta" if encontrado else " no esta"} en la lista')
